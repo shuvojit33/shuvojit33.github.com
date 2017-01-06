132c3d64edcac2cb2b28b9e693488fd064f38131
shuvojit33 <shuvojit33@gmail.com>
2017-01-06 16:59:19 +0600

    fp up

--- script/footprint.js
+++ script/footprint.js
@@ -1,1 +1,2 @@
-console.log(das_footprint + " - Shuvojit Das was here, doing magic! - https://www.fiverr.com/shuvojitdas");
+var dfp = (typeof das_footprint == 'undefined') ? ">>>" : das_footprint;
+console.log(dfp + " ~ Shuvojit Das was here, doing magic! - https://www.fiverr.com/shuvojitdas");
