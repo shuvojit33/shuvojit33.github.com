785fa43715176579934ec6cd523092ea164fac07
shuvojit33 <shuvojit33@gmail.com>
2017-07-06 09:44:52 +0600

    1 step revert

--- script/footprint.js
+++ script/footprint.js
@@ -1,6 +1,2 @@
 var dfp = (typeof das_footprint == 'undefined') ? ">>>" : das_footprint;
 console.log(dfp + " ~ Shuvojit Das was here, doing magic! - https://www.fiverr.com/shuvojitdas");
-
-if(das_footprint=="07042017") {
-console.log("Hello!");
-}
